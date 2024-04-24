23e84a160c8c90c71fbdd2b22c1ed58f9fd18405
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-04-24 17:09:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,149 @@
+// let First = +prompt('Birinchi sonni kiriting')
+
+// let Second = +prompt('Ikkinchi sonni kiriting')
+
+// let Choose = +prompt(`Amalni tanlang:\n1-Qo'shish\n2-Ayirish\n3-Ko'paytirish\n4-Bo'lish\ntanlang: (1/2/3/4)`)
+
+// if (Choose == 1) {
+//     console.log(`${First} + ${Second} = ${First + Second}`);
+// }
+
+
+// else if (Choose == 2) {
+//     console.log(`${First} - ${Second} = ${First - Second}`);
+// }
+
+
+// else if (Choose == 3) {
+//     console.log(`${First} * ${Second} = ${First * Second}`);
+// }
+
+
+// else if (Choose == 4) {
+//     console.log(`${First} / ${Second} = ${First / Second}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let botPlayer = Math.ceil(Math.random() * 3)
+// let userPlayer = +prompt("Son kiriting:\n1-Qaychi\n2-Qogoz\n3-Quduq")
+
+// if (botPlayer === 1) {
+//     console.log('bot: Qaychi');
+// }
+
+
+// else if (botPlayer === 2) {
+//     console.log('bot: Qogoz');
+// }
+
+
+// else if (botPlayer === 3) {
+//     console.log('bot: Quduq');
+// }
+
+
+
+// if (userPlayer === 1) {
+//     console.log('Siz: Qaychi');
+// }
+
+
+// else if (userPlayer === 2) {
+//     console.log('Siz: Qogoz');
+// }
+
+
+// else if (userPlayer === 3) {
+//     console.log('Siz: Quduq');
+// }
+
+
+
+
+
+// if (userPlayer === botPlayer) {
+//     console.log('Durrang');
+// }
+
+
+// else if (userPlayer === 1 && botPlayer === 2) {
+//     console.log('Siz golib boldingiz !');
+// }
+
+
+// else if (userPlayer === 2 && botPlayer === 1) {
+//     console.log('Bot golib boldi');
+// }
+
+
+// else if (userPlayer === 1 && botPlayer === 3) {
+//     console.log('bot golib boldi');
+// } 
+
+
+// else if (userPlayer === 3 && botPlayer === 1) {
+//     console.log('Siz golib boldingiz !');
+// }
+
+
+// else if (userPlayer === 2 && botPlayer === 3) {
+//     console.log('Siz golib boldingiz !');
+// }
+
+
+// else if (userPlayer === 3 && botPlayer === 2) {
+//     console.log('bot golib boldi');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let ball = +prompt('1 dan 100 gacha ball kiriting !')
+
+// if (ball <= 100 && ball > 85) {
+//     console.log('Budject');
+// }
+
+
+// else if (ball <= 85 && ball > 60) {
+//     console.log('contract');
+// }
+
+
+// else if (ball <= 60 && ball > 50) {
+//     console.log('Super contract');
+// }
+
+
+// else if (ball <= 50 && ball > 0) {
+//     console.log('faild');
+// }
+
+
+console.log(confirm('10 ga kopaytirilsinmi ?'));
+    let webBrain = "web" + "Brain ";
+    let IT = 'IT';
+
+    webBrain = webBrain.replace('web', 'IT');
+    webBrain = webBrain.repeat(10)
+
+    console.log(webBrain);
